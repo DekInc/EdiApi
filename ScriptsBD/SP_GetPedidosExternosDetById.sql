0ad065c2b515fab223f1cb83646acd6b9574083b
DekInc <keyfireone@gmail.com>
2019-05-14 09:22:51 -0600

    Actualizo scripts

---
+++ ScriptsBD/SP_GetPedidosExternosDetById.sql
@@ -0,0 +1,41 @@
+USE EdiDB
+GO
+IF OBJECT_ID('SP_GetPedidosExternosDetById', 'P') IS NOT NULL
+	DROP PROC SP_GetPedidosExternosDetById
+GO
+
+CREATE PROCEDURE [dbo].SP_GetGetPaylessProdPrioriByPeriod @Period VARCHAR(10)
+AS
+BEGIN
+	SELECT
+		D.Id,
+		D.IdPAYLESS_ProdPrioriM,
+		D.OID,
+		D.Barcode,
+		D.Estado,
+		D.Pri,
+		D.PoolP,
+		D.Producto,
+		D.Talla,
+		D.Lote,
+		D.Categoria,
+		D.Departamento,
+		D.CP,
+		D.Pickeada,
+		D.Etiquetada,
+		D.Preinspeccion,
+		D.Cargada,
+		D.M3,
+		D.Peso,
+		D.IdTransporte,
+		T.Transporte
+	FROM PAYLESS_ProdPrioriDet D
+	INNER JOIN PAYLESS_ProdPrioriM M
+		ON M.Id = D.IdPAYLESS_ProdPrioriM
+		AND M.Periodo = @Period
+	LEFT JOIN dbo.PAYLESS_Transporte T
+		ON T.Id = D.IdTransporte
+END
+GO
+
+EXEC SP_GetGetPaylessProdPrioriByPeriod '13/05/2019'
